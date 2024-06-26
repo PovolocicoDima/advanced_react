db7acd7d88e541ca073f66eda10fc159b6b6714e
Povolocico Dima <povolocico.dima@gmail.com>
2024-06-26 16:15:10 +0300

    #webpack decomposition: added separated config path with build options for webpack

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,25 @@
+import { Configuration } from "webpack";
+import { buildPlugins } from "./buildPlugins";
+import { buildLoaders } from "./buildLoaders";
+import { buildResolvers } from "./buildResolvers";
+import { BuildOptions } from "./types/config";
+
+
+export function buildWebpackConfig(options: BuildOptions): Configuration {
+    const {paths,  mode} = options
+
+    return {
+        mode: mode,
+        entry: paths.entry,
+        output: {
+            filename: "[name].[contenthash].js",
+            path: paths.build,
+            clean: true
+        },
+        plugins: buildPlugins(paths),
+        module: {
+            rules: buildLoaders()
+        },
+        resolve: buildResolvers(),
+    }
+}
