605cdea16a5d778c135a703c539e63d10fca5bea
Povolocico Dima <povolocico.dima@gmail.com>
2024-07-02 15:31:04 +0300

    added theme styles

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,15 @@
-import './styles/index.scss';
-import { Link } from "react-router-dom";
 import { classNames } from "shared/lib/classNames/classNames";
+import { Navbar } from 'widgets/Navbar';
 import { useTheme } from "./providers/ThemeProvider";
 import { AppRouter } from "./providers/router";
+import './styles/index.scss';
 
 const App = () => {
     const {theme, toggleTheme} = useTheme()
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <Link to={'/about'} >About page</Link>
-            <Link to={'/'}>Main page</Link>
+            <Navbar />
             <button onClick={toggleTheme}>Change theme</button>
             <AppRouter />
         </div>
