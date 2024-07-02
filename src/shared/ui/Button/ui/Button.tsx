d4f69110d10517854af341076e186a5f8c52005e
Povolocico Dima <povolocico.dima@gmail.com>
2024-07-02 17:06:55 +0300

    added shared components, added png, svg:- file loaders to webpack

---
+++ src/shared/ui/Button/ui/Button.tsx
@@ -0,0 +1,23 @@
+
+import { classNames } from 'shared/lib/classNames/classNames'
+import cls from './Button.module.scss'
+import { ButtonHTMLAttributes, FC } from 'react';
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  className?: string;
+  theme: ThemeButton;
+}
+
+export enum ThemeButton {
+    CLEAR = 'clear',
+
+} 
+
+export const Button: FC<ButtonProps> = ({ className, theme, children, ...otherProps }) => {
+  return (
+    <button className={classNames(cls.Button, {}, [className, cls[theme]])} {...otherProps}>
+      {children}
+    </button>
+  )
+};
+
