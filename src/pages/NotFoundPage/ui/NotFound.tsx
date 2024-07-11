cc4af48bbd7b9c91b4649b5e9073bfd02747d6be
Povolocico Dima <povolocico.dima@gmail.com>
2024-07-11 09:39:42 +0300

    new tests and locales config

--- src/pages/NotFoundPage/ui/NotFound.tsx
+++ src/pages/NotFoundPage/ui/NotFound.tsx
@@ -1,10 +1,17 @@
 import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
 import cls from './NotFound.module.scss';
 
 interface NotFoundProps {
   className?: string;
 }
 
-export const NotFound = ({ className } : NotFoundProps) => (
-    <div className={classNames(cls.NotFound, {}, [className])} />
-);
+export const NotFound = ({ className } : NotFoundProps) => {
+    const { t } = useTranslation();
+
+    return (
+        <div className={classNames(cls.NotFound, {}, [className])}>
+            {t('not found page')}
+        </div>
+    );
+};
